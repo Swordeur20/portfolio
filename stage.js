24d29f0332fe0efca321ab9fe442f19b79c7ef48
Anne-Eslie <biekreanneeslie@gmail.com>
2026-06-26 10:38:03 +0200

    feat: new page

--- stage.js
+++ stage.js
@@ -1,3 +1,4 @@
+
 const cards = document.querySelectorAll(".card");
 
 cards.forEach(card => {
@@ -31,16 +32,12 @@ toggleBtn.addEventListener('click', () => {
         document.body.style.backgroundColor = "#081826";
         document.body.style.color = "#ffffff";
         
-        document.querySelector('.propos').style.backgroundColor = "#073A59";
+        document.querySelector('.semaine1').style.backgroundColor = "#073A59";
         document.querySelector('h1.mes-passions').style.backgroundColor = "#073A59";
         document.querySelector('h1.mes-projets').style.backgroundColor = "#073A59";
         document.querySelector('.cs').style.backgroundColor = "#073A59";
-        document.querySelector('.retour').style.backgroundColor = "#049DD9";
         document.querySelectorAll('.card').forEach(card => {
             card.style.backgroundColor = "#073A59";
-            });
-        document.querySelectorAll('.Bouton').forEach(Bouton => {
-            Bouton.style.backgroundColor = "#049DD9";
         });
         
         // On change la couleur de chaque lien trouvé
@@ -50,25 +47,20 @@ toggleBtn.addEventListener('click', () => {
 
     } else {
         // --- MODE JOUR ---
-        document.body.style.backgroundColor = "#5773FF";
+        document.body.style.backgroundColor = "#ffffff";
         document.body.style.color = "#000000";
         
-        document.querySelector('.propos').style.backgroundColor = "#6A81F0";
-        document.querySelector('h1.mes-passions').style.backgroundColor = "#6A81F0";
-        document.querySelector('h1.mes-projets').style.backgroundColor = "#6A81F0";
-        document.querySelector('.cs').style.backgroundColor = "#6A81F0";
-        document.querySelector('.retour').style.backgroundColor = "#CBE2FE";
+        document.querySelector('.semaine1').style.backgroundColor = "#EE2449";
+        document.querySelector('h1.mes-passions').style.backgroundColor = "#EE2449";
+        document.querySelector('h1.mes-projets').style.backgroundColor = "#EE2449";
+        document.querySelector('.cs').style.backgroundColor = "#EE2449";
         document.querySelectorAll('.card').forEach(card => {
-            card.style.backgroundColor = "#6A81F0";
-        });     
-        document.querySelectorAll('.Bouton').forEach(Bouton => {
-            Bouton.style.backgroundColor = "#CBE2FE";
+            card.style.backgroundColor = "#EE2449";
         });
-        
         
         // On change la couleur de chaque lien trouvé
         tousLesLiens.forEach(lien => {
-            lien.style.color = "#000000";
+            lien.style.color = "#073A59";
         });
     }
 });
